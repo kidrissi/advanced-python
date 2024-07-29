4de6dd52ede378ed0690a35d04d74b8d4d558699
kidrissi <k.atiab.idrissi@gmail.com>
2024-07-29 22:05:50 +0100

    print type repr

--- builtinfn/f_print.py
+++ builtinfn/f_print.py
@@ -1,3 +1,4 @@
 print("Python", "version", "3.0", sep="-")  # Output: Python-version-3.0
 print("Hello", end="-")
 print("World!")  # Output: Hello World!
+print(type(repr([1, 2, 3])))  # Output: <class 'str'>
