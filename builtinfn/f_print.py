f854ca89cd7e771e5d272a3e2128fe6aacaaaf22
kidrissi <k.atiab.idrissi@gmail.com>
2024-07-29 22:08:24 +0100

    * unpack all elements

--- builtinfn/f_print.py
+++ builtinfn/f_print.py
@@ -2,3 +2,14 @@ print("Python", "version", "3.0", sep="-")  # Output: Python-version-3.0
 print("Hello", end="-")
 print("World!")  # Output: Hello World!
 print(type(repr([1, 2, 3])))  # Output: <class 'str'>
+
+
+#
+
+def generate_numbers(n):
+    for i in range(n):
+        yield i
+
+
+gen = generate_numbers(5)
+print(*gen)  # Output: 0 1 2 3 4
